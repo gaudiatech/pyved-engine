c1594666a38caa762fd48dcef4f3c749c46c26ff
wkta-tom <tiwaszko@gmail.com>
2022-04-22 18:26:45 +0200

    makin play with fonts more easy

--- src/imgfont-creat-helper.py
+++ src/imgfont-creat-helper.py
@@ -1,59 +1,41 @@
 import katagames_engine as kengi
-kengi.init()
 
 
+kengi.init('old_school')
 pygame = kengi.pygame
 screen = kengi.get_surface()
 width, height = screen.get_size()
 FT_PATH = 'alphbeta.ttf'
-
-INIT_TXT = 'hello user this\nis\nsome\ndope\ntext'
-ALT_TXT = 'i\nunderstand that\nyou watch the console'
-
 pyg_font = pygame.font.Font(FT_PATH, 12)
 
-#custom_ft = kengi.gui.ImgBasedFont('ma_font.png', (220,15,33) )
-
-#print(custom_ft.letter_spacing)
-
 block = kengi.gui.TextBlock(
-    pyg_font, INIT_TXT, (255,0,0)  # pick either pyg_font or custom_ft
+    pyg_font, 'no text', (255, 0, 0)  # pick either pyg_font or custom_ft
 )
 
 block.rect.center = (width // 2, height // 2)  # lets center the text block
 print('*~*~*\npress and hold the space bar ; press ENTER to change alignment')
-ended = False
+can_exit = False
 
-GL_BG_COLOR = (0,0,0)
+GL_BG_COLOR = (0, 0, 0)
+block.debug = 0
 
-# - debug -
-block.debug=0
-
-# pr afficher tous les car!!
+# sert a afficher tous les car. sans exception!
 INIT_TXT = "ABCDEFGHIJKLMNOPQRSTUVWXYZa\nabcdefghijklmnopqrstuvwxyz.\n"
-INIT_TXT += ".-,:+\'!?0\n0123456789()/_=\\[]*\"<>;'\n"
+INIT_TXT += ".-,:+\'!?0\n0123456789()/_=\\[]*\"<>;@$%{}'\n"
 block.text = INIT_TXT
 
-# tmp_ft_part = pygame.Surface((custom_ft.width('hello'), 52))
-#tmp_ft_part.fill((255,255,255))
-# custom_ft.render('hello', tmp_ft_part, (2,2))
-
-# def render(self, text, surf, loc, line_width=0):
-while not ended:
+while not can_exit:
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
-            ended = True
+            can_exit = True
         elif ev.type == pygame.KEYDOWN:
             if ev.key == pygame.K_RETURN:
                 block.text_align = (block.text_align + 1) % 2  # switch text align
-            elif ev.key == pygame.K_SPACE:
-                block.text = ALT_TXT
         elif ev.type == pygame.KEYUP:
             if not pygame.key.get_pressed()[pygame.K_SPACE]:
                 block.text = INIT_TXT
     screen.fill(GL_BG_COLOR)
     block.draw(screen)
-    #screen.blit(tmp_ft_part, (32,168))
-    #custom_ft.render('hello', screen, (2,2), 1)
     kengi.flip()
+
 kengi.quit()
--- src/min-example-gui.py
+++ src/min-example-gui.py
@@ -4,12 +4,16 @@ kengi.init('old_school')
 pygame = kengi.pygame
 screen = kengi.get_surface()
 width, height = screen.get_size()
-FT_PATH = 'alphbeta.ttf'
+
 INIT_TXT = 'hello user this\nis\nsome\ndope\ntext'
 ALT_TXT = 'i\nunderstand that\nyou watch the console'
-ft_obj = pygame.font.Font(FT_PATH, 16)
-# - can use img based font, too
-# ft_obj = kengi.gui.ImgBasedFont('gibson0_font.png', (15, 130, 243))
+USING_REG_FT = True
+
+if USING_REG_FT:
+    FT_PATH = 'alphbeta.ttf'
+    ft_obj = pygame.font.Font(FT_PATH, 16)
+else:
+    ft_obj = kengi.gui.ImgBasedFont('gibson0_font.png', (15, 130, 243))
 
 block = kengi.gui.TextBlock(ft_obj, INIT_TXT, (0, 0, 0))
 block.rect.center = (width // 2, height // 2)  # lets center the text block
