cd0f654f81dc7a5ac93fdbcbd14a6ff8c28d1623
wkta-tom <tiwaszko@gmail.com>
2022-05-29 13:01:54 +0200

    Add draft gametemplate tower-defense

--- game_templates/tower-defense/main.py
+++ game_templates/tower-defense/main.py
@@ -1,3 +1,62 @@
 """
- game template: placeholder
+Game template: placeholder
+
+joint work - tankking & wkta-tom
+
 """
+import glvars
+import katagames_engine as kengi
+kengi.bootstrap_e()
+
+
+# let's use the event manager! game object can receive or send events
+GameObject = kengi.event.EventReceiver
+EngineEvType = kengi.event.EngineEvTypes
+pygame = kengi.pygame
+
+
+class MainDisplay(GameObject):
+    def proc_event(self, ev, source):
+        if ev.type == EngineEvType.PAINT:
+            ev.screen.fill('orange')
+        elif ev.type == EngineEvType.LOGICUPDATE:  # receives a special built-in event that is posted once per frame
+            pass
+        elif ev.type == pygame.QUIT:
+            self.pev(EngineEvType.GAMEENDS)  # posts a special built-in event that stops the game ctrl loop
+
+
+class Mob(GameObject):
+    def __init__(self):
+        super().__init__()
+        self.screen = kengi.get_surface()
+        self.x = self.y = 0
+
+    def proc_event(self, ev, source):
+        if ev.type == EngineEvType.PAINT:
+            pygame.draw.circle(self.screen, 'orange', (self.x, self.y), 15)
+
+
+# - main program
+def run_game():
+    kengi.init('old_school', caption='tower defence', maxfps=30)
+
+    # - set up stuff for using the event system (no ticker)
+    gctrl = kengi.core.get_game_ctrl()
+    gctrl.turn_on()
+
+    # my game objects
+    disp = MainDisplay()
+    disp.turn_on()
+    mobs = [Mob() for _ in range(8)]
+    for m in mobs:
+        m.turn_on()
+
+    # game loop
+    gctrl.loop()
+
+    kengi.quit()
+    print('bye')
+
+
+if __name__ == '__main__':
+    run_game()
---
+++ game_templates/tower-defense/myevents.py
@@ -0,0 +1,8 @@
+
+import katagames_engine as kengi
+
+
+MyEvTypes = kengi.event.enum_ev_types(
+    'MobMoves',  # contains id,x,y
+    'MobDies',  # contains id
+)
