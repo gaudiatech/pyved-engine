478cef244f7950f3da3cebd90d905652fa4a798e
wkta-tom <tiwaszko@gmail.com>
2022-05-30 15:19:41 +0200

    (minor)refactor engine

--- src/katagames_engine/__init__.py
+++ src/katagames_engine/__init__.py
@@ -34,15 +34,129 @@
   easy-to-reuse code!
 """
 
-from . import implem as _imp
+from . import _hub as hub
+from . import pal
+from .Injector import Injector
+from ._BaseGameState import BaseGameState
+from .__version__ import ENGI_VERSION
+from .foundation import defs
+from .foundation import shared  # must keep this line /!\ see web vm
+from .ifaces.pygame import PygameIface
+from .modes import GameModeMger, BaseGameMode
+from .util import underscore_format, camel_case_format
+
+
+ver = ENGI_VERSION
+one_plus_init = False
+_active_state = False
+
+
+class Objectifier:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
+def _show_ver_infos():
+    print(f'KENGI - ver {ENGI_VERSION}, built on top of ')
+
+
+def is_ready():
+    global one_plus_init
+    return one_plus_init
+
+
+def bootstrap_e(info=None):
+    """
+    ensure the engine is ready to be used
+
+    :param info:
+    :return:
+    """
+    global pygame, one_plus_init
+    if one_plus_init:
+        return
+
+    del pygame
+
+    def _ensure_pygame(xinfo):
+        # replace iface by genuine pygame lib, use this lib from now on
+        if isinstance(xinfo, str):
+            hub.kengi_inj.register('pygame', xinfo)
+        else:
+            hub.kengi_inj.set('pygame', xinfo)  # set the module directly, instead of using lazy load
+
+    one_plus_init = True
+    _show_ver_infos()
+    if info is None:
+        info = 'pygame'
+    _ensure_pygame(info)
+
+    # dry import
+    return get_injector()['pygame']
+
+
+def init(gfc_mode='hd', caption=None, maxfps=60, screen_dim=None):
+    global _active_state
+    bootstrap_e()
+    _active_state = True
+    get_injector()['core'].init_e2(gfc_mode, caption, maxfps, screen_dim)
+
+
+def get_surface():
+    global _active_state
+    if not is_ready():
+        raise Exception('calling kengi.get_surface() while the engine isnt ready! (no previous bootstrap op.)')
+    if not _active_state:
+        raise Exception('kengi.init has not been called yet')
+    return get_injector()['core'].get_screen()
+
+
+def flip():
+    return get_injector()['core'].display_update()
+
+
+def quit():  # we keep the "quit" name bc of pygame
+    global _active_state
+
+    if _active_state:
+        get_injector()['event'].EventManager.instance().hard_reset()
+
+        get_injector()['event'].CogObj.reset_class_state()
+
+        get_injector()['core'].init2_done = False
+
+        pyg = get_injector()['pygame']
+        pyg.mixer.quit()
+        pyg.quit()
+
+        _active_state = False
+
+
+def get_injector():
+    return hub.kengi_inj
+
+
+def plugin_bind(plugin_name, pypath):
+    hub.kengi_inj.register(plugin_name, pypath)
+
+
+def bulk_plugin_bind(darg: dict):
+    """
+    :param darg: association extension(plug-in) name to a pypath
+    :return:
+    """
+    for pname, ppath in darg.items():
+        plugin_bind(ppath, ppath)
+
+
+# ----------------------------
+# Stuff related to lazy import
+# ----------------------------
+pygame = PygameIface()
 
 
 def __getattr__(attr_name):
-
-    if attr_name in dir(_imp):
-        return getattr(_imp, attr_name)
-
-    if not _imp.is_ready():
-        raise AttributeError(f"kengi cant use lazy loading, as it hasnt been init yet! (request: {attr_name})")
-
-    return getattr(_imp.hub, attr_name)
+    if not is_ready():
+        raise AttributeError(f"kengi cannot lazy load, it hasnt bootstrap yet! (user request: {attr_name})")
+    else:
+        return getattr(hub, attr_name)
