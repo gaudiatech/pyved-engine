be26056fe24e30d0fe13ff691554eed0a2cf9ec7
wkta-tom <tiwaszko@gmail.com>
2022-06-12 21:04:32 +0200

    add test related to kengi.struct.TreeNode

--- src/katagames_engine/struct.py
+++ src/katagames_engine/struct.py
@@ -76,57 +76,134 @@ class Stack:
         return len(self.fifo_list)
 
 
+# --------------------------------------------
+#  generic tree structure
+# --------------------------------------------
 class Tree:
-    def __init__(self, root_content):
-        self.root = TreeNode(root_content, None)
-        self.__allnodes = set()
-        self.__allnodes.add(self.root)
+    def __init__(self, rootnode_ref):
+        self._root = rootnode_ref
+        self.allnodesinfo = set()  # do not modify this member directly
 
-    def add_content(self, content, parent_node):
-        if parent_node not in self.__allnodes:
-            raise ValueError('cannot find specified parent_node')
+    @property
+    def root(self):
+        return self._root
 
-        n = TreeNode(content, parent_node)
-        parent_node.childs.append(n)
-        self.add_node(n)
+    # TODO
+    # @root.setter
+    # def root(self, noderef):
+    #     self._root = noderef
 
-    def add_node(self, n_val):
-        raise NotImplementedError  # TODO complete this implem.
+    def append_value(self, value, parent_node):
+        if parent_node is None:
+            raise ValueError('[Tree]Error - Cannot use append_value to create a root node!')
+        if parent_node not in self.allnodesinfo:
+            raise ValueError('[Tree]Error - Trying to append value to a node that doesnt exist!')
+        return TreeNode(value, parent_node)
 
+    @property
     def count(self):
-        return len(self.__allnodes)
+        return len(self.allnodesinfo)
 
-    def get_node_by_content(self, searched_content):
-        queue = [self.root]
-        while len(queue) > 0:
-            exn = queue.pop()
-            if searched_content == exn.content:
-                return exn
-            if not exn.is_leaf():
-                queue.extend(exn.childs)
-        return None
+    def has_node(self, node_ref):
+        return node_ref in self.allnodesinfo
 
-    def cut_from_node(self, ref_node):
-        if ref_node == self.root:
-            raise ValueError('empty tree not allowed')
+    def node_by_content(self, needle_value):
+        """
+        returns None if needle_value isnt found
+        """
+        for node_ref in self.allnodesinfo:
+            if needle_value == node_ref.value:
+                return node_ref
 
-        if not ref_node.is_leaf():
-            # if node has childs, we shall cut the whole branch
-            for c in ref_node.childs:
-                self.cut_from_node(c)
+    def sub_tree(self, node_ref):
+        raise NotImplementedError  # TODO, use node cloning
 
-        ref_node.parent.childs.remove(ref_node)
-        self.__allnodes.remove(ref_node)
+    def __str__(self):
+        r = ''
+        for node in self._root.traverse():
+            d = node.depth
+            v = node.value
+            spstr = ''.join([' ' for _ in range(d)])
+            r += f'{spstr}node[depth:{d}] val={v}' + '\n'
+        return r
+
+    # def cut_from_node(self, ref_node):
+    #     if ref_node == self.root:
+    #         raise ValueError('empty tree not allowed')
+    #
+    #     if not ref_node.is_leaf():
+    #         # if node has childs, we shall cut the whole branch
+    #         for c in ref_node.childs:
+    #             self.cut_from_node(c)
+    #
+    #     ref_node.parent.childs.remove(ref_node)
+    #     self.__allnodes.remove(ref_node)
 
 
 class TreeNode:
-    def __init__(self, content, parent):
-        self.childs = list()
-        self.content = content
-        self.parent = parent
+    def __init__(self, value, parent_ref):
+        """
+        set parent_ref to None,
+        in order to create a root node + the associated tree.
+
+        Important remark: this class doesnt allow for the creation of 'headless' nodes,
+        that is nodes that are not bound to a given Tree! It simplifies the implementation
+        as it prevents the dev from doing things that make no sense. For instance: trying to
+        add an existing node (say, the 1st child of root) to the 2nd child of root
+            r
+          /  \
+         a    b
+        doing b.add_child(a) should be always be forbidden,
+        so we dont implement an add_child method.
+        """
+        self.value = value  # data
+
+        if parent_ref:
+            self.parent = parent_ref
+            self.parent.children.append(self)
+            self.tree_ref = parent_ref.tree_ref
+        else:
+            self.parent = None
+            self.tree_ref = Tree(self)
+        self.tree_ref.allnodesinfo.add(self)
+
+        self.children = list()  # references to other nodes
+
+    @property
+    def depth(self):
+        cpt = 0
+        r = self
+        while not r.is_root():
+            r = r.parent
+            cpt += 1
+        return cpt
+
+    @property
+    def child_count(self):
+        return len(self.children)
+
+    def is_root(self):
+        return self.parent is None
 
     def is_leaf(self):
-        return len(self.childs) == 0
+        return len(self.children) == 0
+
+    def remove_child(self, child_node):
+        # removes parent-child relationship
+        if self.tree_ref.has_node(child_node):
+            self.children = [child for child in self.children if child is not child_node]
+            self.tree_ref.allnodesinfo.remove(child_node)
+        else:
+            print('***[TreeNode] warning trying to remove a child that does not exist***')
+
+    def traverse(self):
+        visited_nodes = list()
+        nodes_to_visit = [self]
+        while len(nodes_to_visit) > 0:
+            curr_node = nodes_to_visit.pop(0)
+            visited_nodes.append(curr_node)
+            nodes_to_visit += curr_node.children
+        return visited_nodes
 
 
 class StContainer:
---
+++ tests/struct_tree.py
@@ -0,0 +1,32 @@
+from katagames_engine.struct import TreeNode
+
+
+if __name__ == '__main__':
+    x = TreeNode('salut xy', None)
+    my_tree = x.tree_ref
+    y = TreeNode(88, x)
+    z = TreeNode(15, y)
+
+    print(' should print:\n0 1 2')
+    print(x.depth, y.depth, z.depth)
+
+    print(' should print:\nFalse False True False 1 0')
+    print(y.is_root(), x.is_leaf(), z.is_leaf(), y.is_leaf(), y.child_count, z.child_count)
+
+    print(' should print:\n2 2 5')
+    kappa = TreeNode(128, y)
+    ultima = kappa.tree_ref.append_value(129, kappa)
+    print(kappa.depth, y.child_count, my_tree.count)
+
+    print(' should print:\nsalut xy')
+    print(my_tree.root.value)
+
+    print(' should print:\n-same object ref twice-')
+    print(my_tree.node_by_content(129), ultima)
+
+    print(' should print:\nNone')
+    print(my_tree.node_by_content('lol'))
+
+    print('*****************')
+    print('*****************')
+    print(my_tree)
