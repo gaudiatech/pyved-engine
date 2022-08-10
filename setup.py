71a8675e6d939b0bdd4f4b270c8f5d93b152a65a
wkta-tom <tiwaszko@gmail.com>
2022-08-10 15:59:11 +0200

    fix requirements when install from setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from distutils.core import setup
 import sys
 sys.path.append('src')
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = [] # Here we'll get content of requirements.txt
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
 from katagames_engine.__version__ import ENGI_VERSION
 
 setup(
@@ -23,7 +31,7 @@ setup(
         "katagames_engine.looparts.tmx"
         ],
     include_package_data=True,  # to be sure we get _sm_shelf/legacy.py, etc.
-
+    install_requires=install_requires,
     description='pythonic game engine built on top of pygame',
     license='LGPL3',
     version=str(ENGI_VERSION),
