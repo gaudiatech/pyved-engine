f4567d540713554e10adcf8156e776479c9c1739
wkta-tom <tiwaszko@gmail.com>
2022-08-26 14:27:49 +0200

    version bump

--- src/katagames_engine/__version__.py
+++ src/katagames_engine/__version__.py
@@ -1,7 +1,7 @@
 VYEAR_2DIG = 22  # remember to use only 2 digits
-VMONTH = 7
+VMONTH = 8
 _idx = 1  # 1 =alpha, 2 =beta, 3 =release candidate, 0 =legit release
-VRANK = 1
+VRANK = 2
 
 """
 We target this standard format:
