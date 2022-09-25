2331ce4573159c1fcc12b3480822e28666dddb0a
wkta-tom <tiwaszko@gmail.com>
2022-09-26 01:21:47 +0200

    fix:reactivate (tmx.)ztilemap subsubmodule

--- src/katagames_engine/compo/tmx/__init__.py
+++ src/katagames_engine/compo/tmx/__init__.py
@@ -9,5 +9,14 @@ joint work:
 
 # from . import misc
 from . import data
-# from . import ztilemap
 from . import pytiled_parser
+
+
+_cached_ztm = None
+
+def get_ztilemap_module():
+    global _cached_ztm
+    if not _cached_ztm:
+        from . import ztilemap as _extra_module
+        _cached_ztm = _extra_module
+    return _cached_ztm
