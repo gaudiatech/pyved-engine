3d0233226aa41e9cf5e9f76e673e5f175ebb4554
wkta-tom <tiwaszko@gmail.com>
2022-12-20 17:49:06 +0100

    chess example:play vs AI fixed +using multistate gam approach

--- examples/chess/run_chess.py
+++ examples/chess/run_chess.py
@@ -12,6 +12,8 @@ This is a joint work. Authors are:
 """
 import time
 
+import pygame
+
 import katagames_engine as kengi
 from ChessBoard import ChessBoard
 from ChessGUI_pygame import ChessGameView
@@ -20,8 +22,7 @@ from chess.chess_defs import ChessEvents
 from players import ChessPlayer, ChessAI_random, ChessAI_defense, ChessAI_offense
 
 
-# 0 for normal board setup;
-# see ChessBoard class for other options (for testing purposes)
+# 0 for normal board setup; see ChessBoard class for other options (testing purposes)
 # e.g. the debug mode can use arg 2 instead of 0
 board = ChessBoard(0)
 
@@ -31,6 +32,15 @@ AIvsAI = False
 AIpause = False
 AIpauseSeconds = 0.0
 endgame_msg_added = False
+
+enum_builder = kengi.struct.enum
+gl_screen_ref = None
+EngineEvTypes = kengi.EngineEvTypes
+ChessGstates = enum_builder(
+    'Intro',
+    'Playchess',
+)
+
 
 # constants:
 PAUSE_SEC = 3
@@ -89,7 +99,7 @@ class ChessTicker(kengi.EvListener):
         super().__init__()
         self.refview = refview
         self.refview.board = board
-        self.storedinput = None  # when smth has been played!
+        self.stored_human_input = None  # when smth has been played!
         self._curr_pl_idx = 0
         self._turn_count = 0
         self.endgame_msg_added = False
@@ -98,10 +108,11 @@ class ChessTicker(kengi.EvListener):
         self.endgame_msg_added = False
 
     def on_move_chosen(self, ev):  # as defined in ChessEvents
-        self.storedinput = [ev.from_cell, ev.to_cell]
-        print('-- STORING - -', self.storedinput)
+        self.stored_human_input = [ev.from_cell, ev.to_cell]
+        # print('-- STORING - -', self.stored_human_input)
 
     def on_keydown(self, ev):
+        print('keydown')
         if ev.key == kengi.pygame.K_ESCAPE:
             self.pev(kengi.EngineEvTypes.Gameover)
 
@@ -121,61 +132,67 @@ class ChessTicker(kengi.EvListener):
         self.refview.drawboard(ev.screen, self.board_st)
 
     def on_update(self, ev):
-        global board
+        global board, player
+
         board_st = board.GetState()
+        curr_player = player[self._curr_pl_idx]
 
-        if not self.ready_to_quit:
-
-            # -- the problem with THAT type of code is that it BLOCKS the soft!
-            # if player[currentPlayerIndex].type == 'AI':
-            #     moveTuple = player[currentPlayerIndex].GetMove(board.GetState(), currentColor)
-            # else:
-            #     moveTuple = Gui.GetPlayerInput(board_st, currentColor)
-
-            # TODO repair human vs A.I. play
-
-            moveTuple = self.storedinput
-            if moveTuple:
-                moveReport = board.move_piece(moveTuple)
-                # moveReport = string like "White Bishop moves from A1 to C3" (+) "and captures ___!"
-                self.refview.PrintMessage(moveReport)
-
-                # this will cause the currentPlayerIndex to toggle between 1 and 0
-                self._curr_pl_idx = (self._curr_pl_idx + 1) % 2
-                self.refview.curr_color = ['white', 'black'][self._curr_pl_idx]
-                if AIvsAI and AIpause:
-                    time.sleep(AIpauseSeconds)
-                if rules.IsCheckmate(board.GetState(), player[self._curr_pl_idx].color):
-                    self.ready_to_quit = True
-
-                # ----
-                # - iterate game -
-                currentColor = player[self._curr_pl_idx].color
-                # hardcoded so that player 1 is always white
-                if currentColor == 'white':
-                    self._turn_count = self._turn_count + 1
-                self.refview.PrintMessage("")
-                baseMsg = "TURN %s - %s (%s)" % (str(self._turn_count), player[self._curr_pl_idx].name, currentColor)
-                self.refview.PrintMessage("--%s--" % baseMsg)
-
-                if rules.IsInCheck(board_st, currentColor):
-                    pobj = player[self._curr_pl_idx]
-                    suffx = '{} ({}) is in check'.format(pobj.name, pobj.color)
-                    self.refview.PrintMessage("Warning..." + suffx)
-
-                self.storedinput = None
-
-        else:
-            # --- end game ---
+        if self.ready_to_quit:
+            # --- manage end game ---
             if not self.endgame_msg_added:
                 self.refview.PrintMessage("CHECKMATE!")
                 winnerIndex = (self._curr_pl_idx + 1) % 2
                 self.refview.PrintMessage(
-                    player[winnerIndex].name + " (" + player[winnerIndex].color + ") won the game!")
+                    player[winnerIndex].name + " (" + curr_player.color + ") won the game!")
                 self.endgame_msg_added = True
                 self.refview.EndGame(self.board_st)
+            return
+
+        # -- the problem with THAT type of code is that it BLOCKS the soft!
+        # if player[currentPlayerIndex].type == 'AI':
+        #     moveTuple = player[currentPlayerIndex].GetMove(board.GetState(), currentColor)
+        # else:
+        #     moveTuple = Gui.GetPlayerInput(board_st, currentColor)
+        # TODO repair human vs A.I. play
+
+        move_report = None
+
+        if curr_player.type == 'human':
+            moveTuple = self.stored_human_input
+            if moveTuple:
+                move_report = board.move_piece(moveTuple)
+                # moveReport = string like "White Bishop moves from A1 to C3" (+) "and captures ___!"
+                self.stored_human_input = None
+
+        else:
+            tmp_ai_move = curr_player.GetMove(board_st, curr_player.color)
+            move_report = board.move_piece(tmp_ai_move)
 
         # self.refview.drawboard(gl_screen_ref, board_st)
+
+        if move_report:  # smth has been played!
+            self.refview.PrintMessage(move_report)
+            # this will cause the currentPlayerIndex to toggle between 1 and 0
+
+            # -- iterate game --
+            self._curr_pl_idx = (self._curr_pl_idx + 1) % 2
+            curr_player = player[self._curr_pl_idx]
+            self.refview.curr_color = ['white', 'black'][self._curr_pl_idx]
+            if AIvsAI and AIpause:
+                time.sleep(AIpauseSeconds)
+            if rules.IsCheckmate(board.GetState(), curr_player.color):
+                self.ready_to_quit = True
+
+            currentColor = curr_player.color
+            # hardcoded so that player 1 is always white
+            if currentColor == 'white':
+                self._turn_count = self._turn_count + 1
+            self.refview.PrintMessage("")
+            baseMsg = "TURN %s - %s (%s)" % (str(self._turn_count), curr_player.name, currentColor)
+            self.refview.PrintMessage("--%s--" % baseMsg)
+            if rules.IsInCheck(board_st, currentColor):
+                suffx = '{} ({}) is in check'.format(curr_player.name, curr_player.color)
+                self.refview.PrintMessage("Warning..." + suffx)
 
 
 # parser = OptionParser()
@@ -197,22 +214,74 @@ class ChessTicker(kengi.EvListener):
 # )
 # (giv_options, args) = parser.parse_args()
 
-class MyChessGame(kengi.GameTpl):
 
-    def enter(self, vms=None):
-        global evmanager, gl_screen_ref
-        kengi.init(1)
-        evmanager = kengi.get_ev_manager()
-        evmanager.setup(ChessEvents)
-        self._manager = evmanager
+class DummyV(kengi.EvListener):
+    def __init__(self, go):
+        super().__init__()
+        self.gameobj = go
+
+    def turn_on(self):
+        super().turn_on()
+        print('press RETURN to start the game')
+
+    def on_paint(self, ev):
+        ev.screen.fill('pink')
+
+    def on_keydown(self, ev):
+        if ev.key == pygame.K_RETURN:
+            self.pev(EngineEvTypes.StateChange, state_ident=ChessGstates.Playchess)
+        elif ev.key == pygame.K_ESCAPE:
+            self.gameobj.gameover = True
+        else:
+            print('unknown key pressed [[DummyV cls]]')
+
+
+class IntroState(kengi.BaseGameState):
+    # bind state_id to class is done automatically by kengi (part 1 /2)
+
+    def enter(self):
+        global gl_screen_ref, game_obj
+        self.d = DummyV(game_obj)
+        self.d.turn_on()
+
+    def release(self):
+        self.d.turn_off()
+
+
+class PlaychessState(kengi.BaseGameState):
+    # bind state_id to class is done automatically by kengi (part 1 /2)
+
+    def enter(self):
+        global gl_screen_ref
         initchess()
         gl_screen_ref = kengi.get_surface()
-
         pygamegui = ChessGameView(gl_screen_ref)
-
         ticker = ChessTicker(pygamegui)
         ticker.turn_on()
 
+    def release(self):
+        pass
 
-game_obj = MyChessGame()
+
+kengi.init(1)
+evmanager = kengi.get_ev_manager()
+evmanager.setup(ChessEvents)
+
+kengi.declare_game_states(
+    ChessGstates,  # Warning it will start with the state in this
+    {
+        ChessGstates.Intro: IntroState,
+        # bind state_id to class is done automatically by kengi (part 1 /2)
+        ChessGstates.Playchess: PlaychessState
+    }
+)
+
+
+class DummyCls(kengi.GameTpl):
+    def enter(self, vms=None):
+        global evmanager
+        self._manager = evmanager
+
+
+game_obj = DummyCls()
 game_obj.loop()
