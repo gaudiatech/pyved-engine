33269f1aa07e32f6c8fd29404783a55e88f482a3
wkta-tom <tiwaszko@gmail.com>
2023-07-16 10:13:07 +0200

    polish example platformer so its very close to the new API/declare style

--- examples/platformer0/run_platformer0.py
+++ examples/platformer0/run_platformer0.py
@@ -1,13 +1,13 @@
 """
-this demo is a collab.
-OPEN-SOURCE, public domain
+this demo is a collab - OPEN-SOURCE - Public domain
 
-created by (reverse chronological order)
- -wkta-tom
+Authors, sorted by reverse chronological order:
+
+ -moonbak a.k.a. Tom founder of KataGames - github.com/wkta
  -Renfred Harper
- -Richard Jones (author of the first prototype, incl. xml parsing)
+ -Richard Jones (authored the 1st prototype. This includes XML/TMX parsing)
 
-My goal here in 2022:
+My goal here, in 2022-2023:
 to provide a platformer game template for kengi, refe to:
 github.com/gaudiatech/kengi so every game dev can learn more,
 discover cool stuff that saves time if youre already a pygame user.
@@ -17,21 +17,14 @@ from pyved_engine import *
 from sprites import Player, Enemy
 
 
-# bg = None
-# map_bg_name = None
-# bg_sprite = None
-# first_run = None
-# wanna_quit = False
-# t_last_refresh = None
-
-
+bg_sprite = None
 game = None
 
 
 # -----------------------------------------
 #  two proc used by the game, so the player can respawn
 # -----------------------------------------
-def _session_reset():  # TODO improve design
+def _session_reset(first_run=False):  # TODO improve design
     global game
 
     tilemap_obj = game.tilemap
@@ -59,88 +52,88 @@ def _session_reset():  # TODO improve design
     tilemap_obj.layers.append(enemies)
 
 
-def _game_session():
-    global first_run, wanna_quit, game
-    first_run = False
-    ongoing_session = True
+@pyv.declare_begin
+def prep_game():
+    global game, bg_sprite
 
-    while ongoing_session:
-        dt = pyv.vars.clock.tick(30)
+    HELP_MSG = 'CONTROLS: Left/right arrow key to move | left Ctrl key to shoot | Spacebar to jump'
+    print(HELP_MSG)
 
-        for event in pyv.fetch_events():
-            if event.type == QUIT:
-                wanna_quit = True
-                return
-            if event.type == KEYDOWN and event.key == K_ESCAPE:
-                wanna_quit = True
-                return
-
-        # logic update
-        if game.player.is_dead:
-            ongoing_session = False
-            print('** you died **')
-        else:
-            game.tilemap.update(dt / 1000., game)
-
-        # - display
-        scr = pyv.vars.screen
-        scr.blit(bg.image, (0, 0))
-        game.viewport.draw(scr)
-        pyv.flip()
-
-first_run = True
-def launch():
-    global wanna_quit, map_bg_name, first_run, game, bg_sprite, bg, first_run
     pyv.init()
 
     tilemap_obj = pyv.tmx.data.load_tmx('level.tmx')  # -> Tilemap instance
     li_assets = ['enemy.png', 'missile.png', 'player-right.png', 'player-left.png']
-
+    map_bg_name = ''
     if tilemap_obj.background:
         map_bg_name, map_bg_file = pyv.util.path_to_img_infos(tilemap_obj.background['img_path'])
         li_assets.append(map_bg_file)
-
-    # initialisation pour charger des ressources (snd, images)
-    pyv.preload_assets({
+    pyv.preload_assets({  # chargement ressources
         'images': li_assets,
-        # 'sounds': ['tir1.wav', 'propulseur.wav', 'EXPLOSION.mp3', 'Victoire.mp3', 'musique.mp3']
+        # 'sounds': []
     }, prefix_asset_folder='assets/')
-
-    print('--------- controls ----------')
-    print('left/right arrows to move |  Left Ctrl key to shoot | Spacebar to jump')
-    print()
-    wanna_quit = False
-
-    scr_size = pyv.vars.disp_size
-    viewport = pyv.tmx.misc.Viewport(tilemap_obj, (0, 0), scr_size)
+    viewport = pyv.tmx.misc.Viewport(tilemap_obj, (0, 0), pyv.vars.disp_size)
 
     game = pyv.Objectifier(**{
         'tilemap': tilemap_obj,
+        'viewport': viewport,
         'player': None,
         'sprites': None,
-        'viewport': viewport,
         'enemies': None
     })
 
-    # - search for background
+    # convert bg to a sprite (a sprite is more convenient, as we can repeat/loop it easily)
     if tilemap_obj.background:
-        print('info about bg found in .tmx file, ok!')
-        # turn background image to a sprite, so we can repeat it easily
-        bg = pyv.pygame.sprite.Sprite()
+        # turn background image to
+        tmpbg = pyv.pygame.sprite.Sprite()
         img = pyv.vars.images[map_bg_name]
-        tmp_img = pyv.pygame.transform.scale(img, scr_size)
+        tmp_img = pyv.pygame.transform.scale(img, pyv.vars.disp_size)
+        tmpbg.image = tmp_img
+        tmpbg.rect = tmpbg.image.get_rect()
+        tmpbg.rect.left = -16 + tilemap_obj.background['offsetx']
+        tmpbg.rect.top = 0
 
-        bg.image = tmp_img
-        bg.rect = bg.image.get_rect()
-        bg.rect.left = -16 + tilemap_obj.background['offsetx']
-        bg.rect.top = 0
-        bg_sprite = bg
+        print('bg found in .tmx file: conversion to Sprite ->Ok!')
+        bg_sprite = tmpbg
 
-    _session_reset()
-    while not wanna_quit:
-        _game_session()  # TODO can i do better thant a level-2 game loop?
-        _session_reset()
+    # reset gamesession data
+    _session_reset(first_run=True)
+
+
+@pyv.declare_update
+def update_g():
+    global game
+
+    while True:  # TODO can i do better than this dirty 2nd-order gameloop?
+        dt = pyv.vars.clock.tick(30)
+
+        for event in pyv.fetch_events():
+            if event.type == QUIT:
+                pyv.vars.gameover = True
+                return
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                pyv.vars.gameover = True
+                return
+
+        # logic update
+        if game.player.is_dead:
+            print('** you died **')
+            _session_reset()
+            continue
+
+        game.tilemap.update(dt / 1000., game)
+
+        # - display
+        scr = pyv.vars.screen
+        scr.blit(bg_sprite.image, (0, 0))
+        game.viewport.draw(scr)
+
+        pyv.flip()  # need to flip manually, cause its a 2nd-order gameloop
+
+
+@pyv.declare_end
+def cleanup():
+    print('clean exit!')
     pyv.close_game()
 
 
-launch()
+pyv.run_game()
