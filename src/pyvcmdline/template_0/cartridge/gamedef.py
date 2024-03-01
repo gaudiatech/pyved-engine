a3a55f053956fef6c3f52bc135caae6b1b4ce1ce
wkta-tom <tiwaszko@gmail.com>
2024-03-01 16:29:42 +0100

    replace game template 0 (empty) by smth more interesting

--- src/pyvcmdline/template_0/cartridge/gamedef.py
+++ src/pyvcmdline/template_0/cartridge/gamedef.py
@@ -4,16 +4,32 @@ from . import pimodules
 pyv = pimodules.pyved_engine
 pyv.bootstrap_e()
 pyg = pyv.pygame
+screen = None
+r4 = pyg.Rect(32, 32, 128, 128)
+kpressed = set()
 
 
 @pyv.declare_begin
 def init_game(vmst=None):
-    pyv.init(wcaption='Néant')
+    global screen
+    pyv.init(wcaption='Untitled pyved-based Game')
+    screen = pyv.get_surface()
 
 
 @pyv.declare_update
 def upd(time_info=None):
-    pyv.vars.gameover=True
+    for ev in pyg.event.get():
+        if ev.type == pyg.QUIT:
+            pyv.vars.gameover = True
+        elif ev.type == pyg.KEYDOWN:
+            kpressed.add(ev.key)
+        elif ev.type == pyg.KEYUP:
+            kpressed.remove(ev.key)
+
+    screen.fill('paleturquoise3')
+    if len(kpressed):
+        pyv.draw_rect(screen, 'orange', r4)
+    pyv.flip()
 
 
 @pyv.declare_end
